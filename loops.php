248fb9da566e37dcb36d4832175b2e333592dba9
Paul Thomas <paulthomas2107@gmail.com>
2021-01-31 22:38:11 +0000

    More...

--- loops.php
+++ loops.php
@@ -8,6 +8,31 @@
 <body>
     <?php
 
+    // while
+    $i = 0;
+    while ($i < 10) {
+        echo  ++$i . ', ';
+    }
+    echo '<br>';
+
+    // for loop
+    for($i = 0; $i < 100; $i++) {
+        if (($i % 2) == 0) { // Even numbers
+            continue;
+        } elseif ($i == 37) {
+            break;
+         } else {
+            echo $i. ', '; // Show odds
+         }
+          
+    }
+    // Do while
+    $i = 0;
+    do {
+        echo "Do while < 10: $i<br>";
+        $i++;
+    } while  ($i < 10)
+
 
 
     ?>
