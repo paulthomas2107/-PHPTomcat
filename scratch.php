9f1b18ef6bd0017057676cfbd3c2b2fb2c3f3dac
Paul Thomas <paulthomas2107@gmail.com>
2021-02-01 21:46:38 +0000

    Updated

--- scratch.php
+++ scratch.php
@@ -4,3 +4,10 @@
             echo "$num_1 % $num_2 = " . ($num_1 % $num_2) . "<br>";
             echo "$num_1 / $num_2 = " . (intdiv($num_1,  $num_2)). "<br>";
             
+
+UPDATE mysql.user SET authentication_string='Caitlin1966__' WHERE User='root';
+FLUSH PRIVILEGES;
+exit;
+
+mysql -u root
+ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'Caitlin1966__';
